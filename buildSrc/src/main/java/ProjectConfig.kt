4bec5f4f67fccddfc7727b94e1e74a4e6d7d31ce
Niqrs <niqr.dev@gmail.com>
2022-12-07 21:37:55 +0300

    Migrating to Gradle Kotlin DSL

---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,9 @@
+object ProjectConfig {
+    const val appId = "com.plcoding.calorytracker"
+    const val compileSdk = 33
+    const val minSdk = 26
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val namespace = appId
+}
